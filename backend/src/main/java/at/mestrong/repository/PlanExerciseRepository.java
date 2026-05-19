486b86f30b2c1e7d4c5b4f9d88ee2a1eff76146e
MartinSt23 <martin.staettner23@gmail.com>
2026-05-19 08:22:47 +0200

    PlanDayRepository and PlanExerciseRepository interfaces

---
+++ backend/src/main/java/at/mestrong/repository/PlanExerciseRepository.java
@@ -0,0 +1,11 @@
+package at.mestrong.repository;
+
+import at.mestrong.model.PlanExercise;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface PlanExerciseRepository extends JpaRepository<PlanExercise, Long> {
+    List<PlanExercise> findByPlanDayId(Long dayId);
+}
