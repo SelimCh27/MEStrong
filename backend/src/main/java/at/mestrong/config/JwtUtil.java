fb362c450b4556ead25188b56c1ea91d36b760ce
SelimCh27 <schatti@stundent.tgm.ac.at>
2026-06-08 14:46:01 +0200

    JwtUtil erstellt und fertiggestellt

---
+++ backend/src/main/java/at/mestrong/config/JwtUtil.java
@@ -0,0 +1,50 @@
+package at.mestrong.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import java.security.Key;
+import java.util.Date;
+import java.util.function.Function;
+
+@Component
+public class JwtUtil {
+
+    // In der application.properties definieren: jwt.secret=DeinSehrGeheimesWortDasMindestens256BitLangSeinMuss123!
+    @Value("${jwt.secret:StandardGeheimnisFuerDieEntwicklungDasAusreichendLangIst123!}")
+    private String secret;
+
+    private Key getSigningKey() {
+        return Keys.hmacShaKeyFor(secret.getBytes());
+    }
+
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24)) // 24h
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String extractEmail(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public boolean validateToken(String token, String userEmail) {
+        final String email = extractEmail(token);
+        return (email.equals(userEmail) && !isTokenExpired(token));
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractClaim(token, Claims::getExpiration).before(new Date());
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = Jwts.parserBuilder().setSigningKey(getSigningKey()).build().parseClaimsJws(token).getBody();
+        return claimsResolver.apply(claims);
+    }
+}
